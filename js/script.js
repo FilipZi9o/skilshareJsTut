564788ff673561cec1a2feb1af97a793da0b0fec
KlodovikSofkoPindjur <flpzigo@gmail.com>
2016-02-09 19:52:43 +0100

    dodao script.js i style.css fajlove

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var kolikoKakanja = function (PunoKakanja) {
+	alert ('Danas sam kakao '+PunoKakanja +' puta.' );
+}
+
+var brojkeiSlova = {
+			jedan: 1,
+			dva: 2,
+			tri: 3,
+			slovo: 'a',
+			jelBroj: true,
+			nijeBroj: function() {
+				jelBroj: false;
+			},
+			kaka: function() {
+			alert('kaka!')
+		},
+			
+			
+		}
+
+	console.log('hello there friend!');
